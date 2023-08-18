342d9b20c947b840dd3c683f050f5d2672c74319
bhanu-sh <bhanu1234sharma@gmail.com>
2023-08-18 15:54:07 +0530

    filtering

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,32 +1,93 @@
-import React, { useState } from 'react'
-import laptopList from './dummydata'
+import React, { useEffect, useState } from "react";
+import laptopList from "./dummydata";
 
 const Browse = () => {
-    
-    const [laptopData, setlaptopData] = useState(laptopList);
+  const [laptopData, setLaptopData] = useState(laptopList);
 
-    const displayData = () => {
-        return laptopData.map((laptop) => (
-            <div className='col-md-3'>
-                <div className='card'>
-                    <img className='card-img-top' src={laptop.image} alt="" />
-                    <div className='card-body'>
-                        <h4>{laptop.brand}</h4>
-                        <h3>{laptop.model}</h3>
-                        <h2>&#8377; {laptop.price}</h2>
-                    </div>
-                </div>
-            </div>
-        ))
+  const brands = ["Asus", "Acer", "HP", "Lenovo", "Apple"];
+
+  const [selOptions, setSelOptions] = useState([]);
+
+  const displayData = () => {
+    return laptopData.map((laptop) => (
+      <div className="col-md-3 py-2">
+        <div className="card">
+          <img className="card-img-top" src={laptop.image} alt="" />
+          <div className="card-body">
+            <h4>{laptop.brand}</h4>
+            <h3>{laptop.model}</h3>
+            <h2>&#8377; {laptop.price}</h2>
+          </div>
+        </div>
+      </div>
+    ));
+  };
+
+  const searchLaptop = (e) => {
+    const search = e.target.value;
+    const result = laptopList.filter((laptop) => {
+      return laptop.model.toLowerCase().includes(search.toLowerCase());
+    });
+    setLaptopData(result);
+  };
+
+  const filterBrand = (e) => {
+    if (e.target.value === "") return setLaptopData(laptopList);
+    const selBrand = e.target.value;
+    const result = laptopList.filter((laptop) => {
+      return laptop.brand === selBrand;
+    });
+    setLaptopData(result);
+  };
+
+  const selectOption = (brand) => {
+    if(selOptions.includes(brand)) {
+        setSelOptions(selOptions.filter((b) => b !== brand));
+    }else{
+        setSelOptions([...selOptions, brand]);
     }
+  }
+
+  useEffect(() => {
+    if(selOptions.length === 0) return setLaptopData(laptopList);
+    setLaptopData(laptopList.filter((laptop) => {
+        return selOptions.includes(laptop.brand);
+    }))
+  }, [ selOptions ])
 
   return (
     <div>
-        <div className='container'>
-            <div className='row'>{displayData()}</div>
-        </div>
-    </div>
-  )
-}
+      <header className="bg-dark text-white">
+        <div className="container py-5">
+          <h1 className="text-center">Browse Product</h1>
+          <hr />
+          <input type="text" className="form-control" onChange={searchLaptop} />
 
-export default Browse
+          <div className="row mt-4">
+            <div className="col-md-5">
+              <select className="form-control w-25 mt-4" onChange={filterBrand}>
+                <option value="">Select Brand</option>
+                {brands.map((b) => (
+                  <option value={b}>{b}</option>
+                ))}
+              </select>
+            </div>
+            
+            <div className="col-md-4 my-auto">
+                <input checked={(selOptions.includes('Asus'))} onClick={() => {selectOption('Asus')}} className="form-check-input" type="checkbox" /> Asus&nbsp;&nbsp;&nbsp;
+                <input checked={(selOptions.includes('HP'))} onClick={() => {selectOption('HP')}} className="form-check-input" type="checkbox" /> HP&nbsp;&nbsp;&nbsp;
+                <input checked={(selOptions.includes('Acer'))} onClick={() => {selectOption('Acer')}} className="form-check-input" type="checkbox" /> Acer&nbsp;&nbsp;&nbsp;
+                <input checked={(selOptions.includes('Lenovo'))} onClick={() => {selectOption('Lenovo')}} className="form-check-input" type="checkbox" /> Lenovo&nbsp;&nbsp;&nbsp;
+            </div>
+          </div>
+        </div>
+      </header>
+
+      <div className="container">
+        <div className="row">{displayData()}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Browse;
--- src/components/dummydata.js
+++ src/components/dummydata.js
@@ -9,30 +9,51 @@ const laptopList = [
     {
         id: 3654,
         brand: 'Asus',
-        model: 'Vivobook 16',
+        model: 'Zenbook',
         price: 54000,
-        image: 'https://dlcdnwebimgs.asus.com/gain/31736f29-2307-4583-b887-a9fc5a85f259/'
+        image: 'https://dlcdnwebimgs.asus.com/gain/de9b7594-6143-4370-a14c-7220ac3b08c6/'
     },
     {
         id: 3654,
         brand: 'Asus',
-        model: 'Vivobook 16',
+        model: 'ROG Strix',
         price: 54000,
-        image: 'https://dlcdnwebimgs.asus.com/gain/31736f29-2307-4583-b887-a9fc5a85f259/'
+        image: 'https://dlcdnwebimgs.asus.com/gain/374267AB-B810-436C-8C2B-577ECA70267A/w717/h525'
     },
     {
         id: 3654,
         brand: 'Asus',
-        model: 'Vivobook 16',
+        model: 'Tuf Gaming',
         price: 54000,
-        image: 'https://dlcdnwebimgs.asus.com/gain/31736f29-2307-4583-b887-a9fc5a85f259/'
+        image: 'https://www.asus.com/media/global/gallery/vkhlvwgqqpvrnhwu_setting_xxx_0_90_end_2000.png'
     },
     {
         id: 3654,
-        brand: 'Asus',
-        model: 'Vivobook 16',
+        brand: 'Acer',
+        model: 'Predator',
         price: 54000,
-        image: 'https://dlcdnwebimgs.asus.com/gain/31736f29-2307-4583-b887-a9fc5a85f259/'
+        image: 'https://media-ik.croma.com/prod/https://media.croma.com/image/upload/v1680947875/Croma%20Assets/Computers%20Peripherals/Laptop/Images/271671_2_mrlon4.png'
+    },
+    {
+        id: 3654,
+        brand: 'HP',
+        model: 'Pavilion',
+        price: 54000,
+        image: 'https://img-prd-pim.poorvika.com/cdn-cgi/image/width=500,height=500,quality=75/product/HP-Pavilion-Gaming-Intel-Core-i5-10th-Gen-Windows-10-Home-Laptop-15-dk1508TX-8GB-512GB-Shadow-Black.png'
+    },
+    {
+        id: 3654,
+        brand: 'Lenovo',
+        model: 'Ideapad',
+        price: 54000,
+        image: 'https://media-ik.croma.com/prod/https://media.croma.com/image/upload/v1690469210/Croma%20Assets/Computers%20Peripherals/Laptop/Images/274578_wdjl0x.png'
+    },
+    {
+        id: 3654,
+        brand: 'Apple',
+        model: 'Macbook',
+        price: 54000,
+        image: 'https://d2xamzlzrdbdbn.cloudfront.net/products/71fb2d93-2fdc-497c-8790-a6d6fd64a0ba22161347.jpg'
     }
 ]
 
